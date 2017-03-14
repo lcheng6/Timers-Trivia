03c33ead6db87f314110eb8223c1adc7f4b465d4
Liang Cheng <liangcheng6@gmail.com>
2017-03-14 17:44:21 -0400

    initial commit of trivia levels

---
+++ assets/js/gamelevel.js
@@ -0,0 +1,37 @@
+'use strict'
+//the constructor for gameLevel object, 
+//triviaQuestion: the text of the trivia question
+//triviaChoices: an array of guesses, a plain array with 4 strings. 
+//correctAnswerIndex: is the index of the correct answer within triviaChoices
+var gameLevel = function(triviaQuestion, triviaChoices, correctAnswerIndex, imgCard) {
+	var priv = {
+		question: triviaQuestion,
+		choices: triviaChoices,
+		correctAnswerIndex: correctAnswerIndex, 
+		imgCard: imgCard
+	};
+
+	return {
+		//return the Trivia's question
+		getTriviaQuestion: function() {
+			return priv.Question;
+		},
+
+		//return true is guessed correct, otherwise not
+		isAnswerCorrect: function(index) {
+			if(priv.correct === index) {
+				return true;
+			}else {
+				return false
+			}
+		},
+
+		//return the list of choices for this trivia
+		getChoices: function() {
+			return priv.choices;
+		},
+		getImgCard: function() {
+			return priv.imgCard;
+		}
+	}
+}
